a3a9633471b5844760d10e72bf64ffe21608d8f5
Robson Maia <robsonmvieira@gmail.com>
2021-06-03 23:12:37 -0300

    :art: split connection config

---
+++ category/src/infrastructure/configs/database.config.ts
@@ -0,0 +1,11 @@
+// import * as typeormConfig from './orm.config'
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const database: any = {
+//   ...typeormConfig,
+//   migrations: ['./src/infrastructure/database/migrations/*.ts'],
+//   cli: { migrationsDir: './src/infrastructure/database/migrations' },
+//   entities: ['./src/modules/**/database/*.entity.ts']
+// }
+// // migrationsTableName: 'migrations',
+
+// export default database
---
+++ category/src/infrastructure/configs/orm.config.ts
@@ -0,0 +1,13 @@
+// import { config } from 'dotenv'
+// import { ConnectionOptions } from 'typeorm'
+
+// config()
+// export = {
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: Number.parseInt(process.env.DB_PORT as string, 10),
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   autoLoadEntities: true
+// } as ConnectionOptions
