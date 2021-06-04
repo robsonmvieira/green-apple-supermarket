ef616f8a468729b15ca0077e36d7bdbed42c570c
Robson Maia <robsonmvieira@gmail.com>
2021-06-03 23:24:35 -0300

    :sparkles: add FindCategoryUseCase

--- category/src/modules/category/usecases/find-categories/find-categories.usecase.ts
+++ category/src/modules/category/usecases/find-categories/find-categories.usecase.ts
@@ -1,8 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 
 import { ICategoryRepository } from '@infrastructure/repositories/category/category.interface.repository'
-import { CategoryEntity } from '@modules/category/domain/entities/category.entity'
-// import { CategoryResponse } from '@modules/category/dtos/category.response.dto'
+import { CategoryOrmEntity } from '@modules/category/database/category-orm.entity'
 
 @injectable()
 export class FindCategoryUseCase {
@@ -11,7 +10,7 @@ export class FindCategoryUseCase {
     private categoryRepository: ICategoryRepository
   ) {}
 
-  async execute(): Promise<CategoryEntity[]> {
+  async execute(): Promise<CategoryOrmEntity[]> {
     return this.categoryRepository.findAll()
   }
 }
