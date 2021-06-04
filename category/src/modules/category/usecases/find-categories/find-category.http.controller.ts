a3ed767c122ba13a90c508aaff0ba325442b43fe
Robson Maia <robsonmvieira@gmail.com>
2021-06-03 23:24:00 -0300

    :sparkles: add FindCategoryHttpController

--- category/src/modules/category/usecases/find-categories/find-category.http.controller.ts
+++ category/src/modules/category/usecases/find-categories/find-category.http.controller.ts
@@ -6,7 +6,6 @@ import { FindCategoryUseCase } from './find-categories.usecase'
 export class FindAllCategoryHttpController {
   async handle(req: Request, res: Response): Promise<Response> {
     const useCase = container.resolve(FindCategoryUseCase)
-
     const categories = await useCase.execute()
 
     return res.status(200).json(categories)
