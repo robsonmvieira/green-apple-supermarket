10390bead5e20dabce5ea6d20a9329ba1d06fb7e
Robson Maia <robsonmvieira@gmail.com>
2021-06-03 23:10:00 -0300

    :sparkles: update index root file

--- category/src/server.ts
+++ category/src/server.ts
@@ -1,19 +1,26 @@
 import { config } from 'dotenv'
+import '@infrastructure/database'
 import express from 'express'
 import morgan from 'morgan'
-import './consumer'
+
+import 'express-async-errors'
+import 'reflect-metadata'
+import routes from '@infrastructure/configs/routes'
+import errorInterceptor from '@infrastructure/interceptors/error.interceptor'
+
+// import './consumer'
+import '@infrastructure/configs/dependency-injection-container'
 
 config()
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
-// import './categoryProducer'
+
+app.use('/', routes)
 
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'ok' })
-})
+app.use(errorInterceptor)
 
 app.listen(port, () =>
   console.log('category api running on http://localhost:8001')
