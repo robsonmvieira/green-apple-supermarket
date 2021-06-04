b81b665296c89f0ec1c1e7da6480ca26e26e3fad
Robson Maia <robsonmvieira@gmail.com>
2021-06-03 23:10:50 -0300

    :hammer: update entityBase

--- category/src/core/base-classes/entity.base.ts
+++ category/src/core/base-classes/entity.base.ts
@@ -1,10 +1,36 @@
-export abstract class Entity {
-  id: string
-  createdAt: number
-  updatedAt: number
+import { v4 as uuidV4 } from 'uuid'
 
-  constructor() {
-    this.createdAt = Date.now()
-    this.updatedAt = Date.now()
+import { DateVO } from '@core/value-objects/date.value-object'
+
+export abstract class Entity<EntityProps> {
+  id: string
+  private readonly _createdAt: DateVO
+  private _updatedAt: DateVO
+  props: EntityProps
+  constructor(props: EntityProps) {
+    this.id = uuidV4()
+    const now = DateVO.now()
+
+    this._createdAt = now
+    this._updatedAt = now
+    this.props = props
+  }
+
+  get createdAt(): string {
+    return this._createdAt.value
+  }
+  get updatedAt(): string {
+    return this._updatedAt.value
+  }
+
+  public toObject(): unknown {
+    const propsCopy = { ...this.props }
+    const result = {
+      id: this.id,
+      createdAt: this._createdAt.value,
+      updatedAt: this._updatedAt.value,
+      ...propsCopy
+    }
+    return result
   }
 }
