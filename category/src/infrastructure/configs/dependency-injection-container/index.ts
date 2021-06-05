a1af1cdef017180131a71368971623d3c69221ec
Robson Maia <robsonmvieira@gmail.com>
2021-06-05 10:09:35 -0300

    :sparkles: update DI container

--- category/src/infrastructure/configs/dependency-injection-container/index.ts
+++ category/src/infrastructure/configs/dependency-injection-container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe'
 
+import { IOnCategoryCreatedDomainEventHandler } from '@core/domain-events/domain-event-handler.interface'
 import { ICategoryRepository } from '@infrastructure/repositories/category/category.interface.repository'
 import { CategoryRepository } from '@modules/category/database/category.repository'
+import { OnCategoryCreatedDomainEventHandler } from '@modules/category/domain-event-handlers/category-created.event-handler'
 
 container.registerSingleton<ICategoryRepository>(
   'ICategoryRepository',
   CategoryRepository
+)
+
+container.registerSingleton<IOnCategoryCreatedDomainEventHandler>(
+  'IOnCategoryCreatedDomainEventHandler',
+  OnCategoryCreatedDomainEventHandler
 )
