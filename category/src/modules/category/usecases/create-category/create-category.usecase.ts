d9eb8492be2c5406ab1b8f0933e909839848957d
Robson Maia <robsonmvieira@gmail.com>
2021-06-05 10:14:12 -0300

    :sparkles: update createCategoryUseCase

--- category/src/modules/category/usecases/create-category/create-category.usecase.ts
+++ category/src/modules/category/usecases/create-category/create-category.usecase.ts
@@ -1,20 +1,31 @@
 import { inject, injectable } from 'tsyringe'
 
+import { IOnCategoryCreatedDomainEventHandler } from '@core/domain-events/domain-event-handler.interface'
 import { ICategoryRepository } from '@infrastructure/repositories/category/category.interface.repository'
 import { CategoryOrmEntity } from '@modules/category/database/category-orm.entity'
 import { CategoryEntity } from '@modules/category/domain/entities/category.entity'
+import { CategoryCreatedDomainEvent } from '@modules/category/domain/events/category-created.domain-event'
 import { CreateCategoryDto } from '@modules/category/dtos/create-category.dto'
 
 @injectable()
 export class CreateCategoryUseCase {
   constructor(
     @inject('ICategoryRepository')
-    private categoryRepository: ICategoryRepository
+    private categoryRepository: ICategoryRepository,
+    @inject('IOnCategoryCreatedDomainEventHandler')
+    private domainHandler: IOnCategoryCreatedDomainEventHandler
   ) {}
 
   async execute({ name }: CreateCategoryDto): Promise<CategoryOrmEntity> {
     const newCategory = new CategoryEntity({ name }).toObject()
     const categoryOrm = Object.assign(newCategory, new CategoryOrmEntity())
-    return this.categoryRepository.create(categoryOrm)
+    const result = await this.categoryRepository.create(categoryOrm)
+    const categoryEvent = new CategoryCreatedDomainEvent({
+      id: result.id,
+      name: result.name
+    })
+    this.domainHandler.onCategoryCreated(categoryEvent)
+
+    return result
   }
 }
