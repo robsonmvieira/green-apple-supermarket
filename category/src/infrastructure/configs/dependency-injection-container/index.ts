70db4e566d3ef1c0d11418a2fb75f78aee6e4f6d
Robson Maia <robsonmvieira@gmail.com>
2021-06-07 18:49:49 -0300

    :hammer: update DI Container

--- category/src/infrastructure/configs/dependency-injection-container/index.ts
+++ category/src/infrastructure/configs/dependency-injection-container/index.ts
@@ -1,9 +1,13 @@
 import { container } from 'tsyringe'
 
+import { IOnCategoryDeletedDomainEventHandler } from '@core/domain-events/domain-event-category-deleted-handler.interface'
+import { IOnCategoryUpdatedDomainEventHandler } from '@core/domain-events/domain-event-category-updated-handler.interface'
 import { IOnCategoryCreatedDomainEventHandler } from '@core/domain-events/domain-event-handler.interface'
 import { ICategoryRepository } from '@infrastructure/repositories/category/category.interface.repository'
 import { CategoryRepository } from '@modules/category/database/category.repository'
 import { OnCategoryCreatedDomainEventHandler } from '@modules/category/domain-event-handlers/category-created.event-handler'
+import { OnCategoryDeletedDomainEventHandler } from '@modules/category/domain-event-handlers/category-deleted.event-handler'
+import { OnCategoryUpdatedDomainEventHandler } from '@modules/category/domain-event-handlers/category-updated.event-handler'
 
 container.registerSingleton<ICategoryRepository>(
   'ICategoryRepository',
@@ -13,4 +17,14 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<IOnCategoryCreatedDomainEventHandler>(
   'IOnCategoryCreatedDomainEventHandler',
   OnCategoryCreatedDomainEventHandler
+)
+
+container.registerSingleton<IOnCategoryDeletedDomainEventHandler>(
+  'IOnCategoryDeletedDomainEventHandler',
+  OnCategoryDeletedDomainEventHandler
+)
+
+container.registerSingleton<IOnCategoryUpdatedDomainEventHandler>(
+  'IOnCategoryUpdatedDomainEventHandler',
+  OnCategoryUpdatedDomainEventHandler
 )
