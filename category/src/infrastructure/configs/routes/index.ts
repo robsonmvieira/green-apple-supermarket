f81449cf6c9614798f771788e8eb0865f0d2ac4a
Robson Maia <robsonmvieira@gmail.com>
2021-06-07 18:50:07 -0300

    :hammer: update root routes

--- category/src/infrastructure/configs/routes/index.ts
+++ category/src/infrastructure/configs/routes/index.ts
@@ -5,19 +5,23 @@ import { FindAllCategoryHttpController } from '@modules/category/usecases/find-c
 import { FindCategoryByIdHttpController } from '@modules/category/usecases/find-category-by-id/find-category-by-id.http.controller'
 import { FindCategoryByNameHttpController } from '@modules/category/usecases/find-category-by-name/find-category-by-name.http.controller'
 import { RemoveCategoryHttpController } from '@modules/category/usecases/remove-category/remove-category.http.controller'
+import { UpdateCategoryHttpController } from '@modules/category/usecases/update-category/update-category.http.controller'
 
 const routes = Router()
 const findCategoriesHttpController = new FindAllCategoryHttpController()
-const createCategoryHttpController = new CreateCategoryHttpController()
-const findCategoryByIdHttpController = new FindCategoryByIdHttpController()
 const findCategoryByNameHttpController = new FindCategoryByNameHttpController()
+const findCategoryByIdHttpController = new FindCategoryByIdHttpController()
+
+const createCategoryHttpController = new CreateCategoryHttpController()
+
+const updateCategoryHttpController = new UpdateCategoryHttpController()
 const removeCategoryByIdHttpController = new RemoveCategoryHttpController()
 
 routes.post('/', createCategoryHttpController.handle)
 routes.get('/', findCategoriesHttpController.handle)
 routes.get('/:id', findCategoryByIdHttpController.handle)
 routes.get('/:name', findCategoryByNameHttpController.handle)
-// routes.put('/:id', authInterceptor, updateUserHttpController.handle)
+routes.put('/:id', updateCategoryHttpController.handle)
 // routes.delete('/:id', authInterceptor, removeCategoryByIdHttpController.handle)
 routes.delete('/:id', removeCategoryByIdHttpController.handle)
 
