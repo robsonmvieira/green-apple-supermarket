dec197514899e916623f7ace1d6af934c5d002d8
Robson Maia <robsonmvieira@gmail.com>
2021-07-04 20:59:33 -0300

    :sparkles: multer file helper

---
+++ product/src/infrastructure/configs/utils/upload.ts
@@ -0,0 +1,17 @@
+import crypto from 'crypto'
+import multer from 'multer'
+import { resolve } from 'path'
+
+const tmpFolder = resolve(__dirname, '..', '..', '..', '..', 'tmp')
+export default {
+  tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, cb) => {
+      const fileHash = crypto.randomBytes(16).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
+
+      return cb(null, fileName)
+    }
+  })
+}
